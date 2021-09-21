632346d8e0b3d22e6417362d0629342de8839b51
Brent Groves <brent.groves@gmail.com>
2021-09-21 11:18:11 -0400

    Updated Sql connection test script

--- SSIS/Test-SqlConnection.ps1
+++ SSIS/Test-SqlConnection.ps1
@@ -1,28 +1,34 @@
 # https://mcpmag.com/articles/2018/12/10/test-sql-connection-with-powershell.aspx
-
-param ($ServerName, $DatabaseName,$userName,$psw)
+# powershell.one
+param ($server, $database,$user,$password,$port) 
 
 function Test-SqlConnection {
     param(
         [Parameter(Mandatory)]
-        [string]$ServerName,
+        [string]$server,
 
         [Parameter(Mandatory)]
-        [string]$DatabaseName,
+        [string]$database,
 
         [Parameter(Mandatory)]
-        [string]$userName,
+        [string]$user,
 
         [Parameter(Mandatory)]
-        [string]$psw
+        [string]$password,
+
+        [string]$port
     )
-
+# Causes error to throw exception
     $ErrorActionPreference = 'Stop'
 
     try {
         # write-host "Start"
-
-        $connectionString = 'Data Source={0};database={1};User ID={2};Password={3}' -f $ServerName,$DatabaseName,$userName,$psw
+        $serverPort = $server
+        if($port -ne $null)
+        {
+            $serverPort += ',' + $port
+        }
+        $connectionString = 'Data Source={0};database={1};User ID={2};Password={3}' -f $serverPort,$database,$user,$password
         $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $ConnectionString
         $sqlConnection.Open()
         ## This will run if the Open() method does not throw an exception
@@ -40,7 +46,13 @@ function Test-SqlConnection {
 }
 #  "if (Test-Path '" + @[User::TestFilePath] + "') {exit 0} else {exit 999}\
 # Test-SqlConnection -ServerName 'busche-sql' -DatabaseName 'Busche ToolList' -userName 'sa' -psw 'buschecnc1'
-$retcode = Test-SqlConnection -ServerName $ServerName -DatabaseName $DatabaseName -userName $userName -psw $psw
+if($null -ne $port)
+{
+    $retcode = Test-SqlConnection -server $server -database $database -user $user -password $password -port $port
+} else {
+    $retcode = Test-SqlConnection -server $server -database $database -user $user -password $password 
+}
+
 if ($retcode -eq $true) 
 {
     exit 0
@@ -49,3 +61,5 @@ if ($retcode -eq $true)
 }
 # Write-Host $retcode
 # powershell.exe -NoProfile -ExecutionPolicy ByPass C:\SSIS\Test-SqlConnection.ps1 -ServerName 'busche-sql' -DatabaseName 'Busche ToolList' -userName 'sa' -psw 'buschecnc1'
+# mgsqlmi.public.48d444e7f69b.database.windows.net 
+# . 'c:\Users\bgroves\source\repos\ps_tools\SSIS\Test-SqlConnection.ps1' -ServerName 'mgsqlmi.public.48d444e7f69b.database.windows.net,3342' -DatabaseName 'mgdw' -userName 'mgadmin' -psw 'WeDontSharePasswords1!'
---
+++ common/Test-Connection.ps1
@@ -0,0 +1,3 @@
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-connection?view=powershell-7.1
+# mgsqlmi.public.48d444e7f69b.database.windows.net 
+# . 'c:\Users\bgroves\source\repos\ps_tools\SSIS\Test-SqlConnection.ps1' -ServerName 'busche-sql' -DatabaseName 'Busche ToolList' -userName 'sa' -psw 'buschecnc1'

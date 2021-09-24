5525c842de2889607186c332095a9436b5661160
Brent Groves <brent.groves@gmail.com>
2021-09-24 17:26:31 -0400

    edon host

--- common/MSCEdonHostBG.ps1
+++ common/MSCEdonHostBG.ps1
@@ -1,1 +1,3 @@
-Invoke-Command -ComputerName ALB-60409WJ-IT -ScriptBlock { Test-Connection -ComputerName MSCEDONHOSTBG } -credential bgroves
+# Invoke-Command -ComputerName ALB-60409WJ-IT -ScriptBlock { Test-Connection -ComputerName MSCEDONHOSTBG } -credential bgroves?
+# Invoke-Command -ComputerName ALB-60409WJ-IT -ScriptBlock { Get-DnsClientServerAddress } -credential bgroves | Select-Object ServerAddress
+Invoke-Command -ComputerName ALB-60409WJ-IT -ScriptBlock { Get-NetIPAddress } -credential bgroves
